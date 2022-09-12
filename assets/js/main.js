7676b76d9a3242bfef4689580a6fefa71ee89cb0
Juanchis <juanchismo10@gmail.com>
2022-09-11 21:50:04 -0300

    Creación de la pantalla para un nuevo juego

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,12 @@ const btnStartGame = document.querySelector('.init')
 const btnStartAdd = document.querySelector('.add')
 const divButtons = document.querySelector('.buttons')
 const addWord = document.querySelector('.addWord')
+const sectionNewgame = document.querySelector('.new__game')
 
 // click to start game
 btnStartGame.addEventListener('click', () => {
     addOrRemoveClassToElement(divButtons, 'hidden')
+    addOrRemoveClassToElement(sectionNewgame, 'hidden')
 })
 
 // click to add word
